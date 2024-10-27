2da311e434c1f35bcad2da0bec21c1b0300b7ecb
Verma <aashishverma239@gmail.com>
2024-10-27 22:25:33 +0530

    half pyramid

--- half_pyramid.c
+++ half_pyramid.c
@@ -6,10 +6,13 @@ int main() {
    scanf("%d",&n);
    
    for(i=1; i<=n; i++){
+    
        for(j=1; j<=i; j++){
            printf("*");
        }
+
        printf("\n");
+
    }
 
     return 0;
